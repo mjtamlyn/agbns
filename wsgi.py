d5b3cc6a08b06005203131e14bc850f99b42b6a7
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-07-14 22:00:16 +0100

    Use correct host to bind to.

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ import static
 from wsgiref.simple_server import make_server
 
 
-make_server('localhost', int(os.environ.get('PORT', 8000)), static.Cling('.')).serve_forever()
+make_server('0.0.0.0', int(os.environ.get('PORT', 8000)), static.Cling('.')).serve_forever()
