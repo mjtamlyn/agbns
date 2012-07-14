7057683f078e5d0c8581d9a5abd69bbd940dd09e
Marc Tamlyn <marc.tamlyn@gmail.com>
2012-07-14 21:56:13 +0100

    Make the port an integer...

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ import static
 from wsgiref.simple_server import make_server
 
 
-make_server('localhost', os.environ.get('PORT', 8000), static.Cling('.')).serve_forever()
+make_server('localhost', int(os.environ.get('PORT', 8000)), static.Cling('.')).serve_forever()
